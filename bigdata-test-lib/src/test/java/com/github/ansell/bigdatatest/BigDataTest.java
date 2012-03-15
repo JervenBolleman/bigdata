8807ed37aaecada34ff2656b1c29af562c07c6da
Peter Ansell <p_ansell@yahoo.com>
2012-03-15 16:55:10 +1000

    first draft, not working yet

---
+++ bigdata-test-lib/src/test/java/com/github/ansell/bigdatatest/BigDataTest.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.github.ansell.bigdatatest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.bigdata.rdf.sail.BigdataSail;
+import com.bigdata.rdf.sail.BigdataSailRepository;
+
+/**
+ * @author Peter Ansell p_ansell@yahoo.com
+ *
+ */
+public class BigDataTest
+{
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+        BigdataSail sail = new BigdataSail();
+        
+        BigdataSailRepository testRepo = new BigdataSailRepository(sail);
+    }
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+    
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+    
+}
